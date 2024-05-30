ed5d5805131f8c68c462dffb83b611f0f91de667
Shakinthan Sivalingam <144006288+shsi003@users.noreply.github.com>
2024-05-30 16:44:07 +0200

    finished html

--- js/app.js
+++ js/app.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	const form = document.getElementById('productRegistry');
 	const registry = document.getElementById('registry');
 
+	//Retrieves registered products from local storage
+	const storedProducts = JSON.parse(localStorage.getItem('products')) || [];
+
+	//display stored products
 	
+
 })
