454d140f2a84470088517c2900df7a8f5c2ebf0d
Shakinthan Sivalingam <144006288+shsi003@users.noreply.github.com>
2024-05-30 16:41:07 +0200

    finished hmtl

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('productRegistry');
+	const registry = document.getElementById('registry');
+
+	
+})
